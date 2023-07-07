4e7996e73a1a5fabf4210e17af5bfed8203007db
EderArthur <eder.arthur12345@gmail.com>
2023-07-07 00:54:50 -0300

    ListaSuspensa

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,7 +1,20 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times =  [
+        'Progaramação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'inovação e Gestão'
+    ]
+    
+
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +22,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label ="Time" itens = {times}/>
             </form>
         </section>
     )
