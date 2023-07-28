508c970a4c892a2e6d65a3ef88b3fbfd4b431eff
EderArthur <eder.arthur12345@gmail.com>
2023-07-28 00:29:42 -0300

    att olá mundo

--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 
 function AppRoutes() {
@@ -9,10 +11,33 @@ function AppRoutes() {
     <BrowserRouter>
       <Menu></Menu>
       <Routes>
-        <Route path="/" element={<Inicio/>}/>
-        <Route path="/sobremim" element={<SobreMim/>}/>
+        <Route path="/" element={<PaginaPadrao/>}>
+          <Route path="/" element={<Inicio/>}/>
+          <Route path="/sobremim" element={<SobreMim/>}/>
+        </Route>
+
+        {/* 
+        
+        Na rota “/”, a estrutura a ser renderizada é:
+
+        <PagnaPadrao>
+          <Inicio/>
+        <PaginaPadrao/>
+
+        Na rota “/sobremim”, a estrutura a ser renderizada é:
+
+        <PagnaPadrao>
+          <SobreMim/>
+        <PaginaPadrao/>
+        
+        */}
+
+        
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+
+      <Rodape/>
+
     </BrowserRouter>
   );
 }
